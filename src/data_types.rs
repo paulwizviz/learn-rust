a2070795339e0fbb03a30404eda35835c478fe47
Paul Sitoh <paulwizviz@gmail.com>
2021-07-10 15:55:06 +0100

    Update integer type example
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- src/data_types.rs
+++ src/data_types.rs
@@ -6,14 +6,29 @@ pub fn integers(){
     let int: isize = -32;
     println!("signed architecture dependent int {}", int);
 
-    let unsigned_int: u8 = 1;
-    println!("unsigned 8-bit {}", unsigned_int);
+    let unsigned_int8: u8 = 255;
+    println!("unsigned int8 {}", unsigned_int8);
 
-    let signed_int: i8 = -1;
-    println!("signed int 8-bit {}", signed_int);
+    let signed_int8: i8 = -128;
+    println!("signed int8 {}", signed_int8);
 
-    let visual_sep_int: i32 = 256_000;
-    println!("visually separate int {}", visual_sep_int);
+    let signed_int32: i32 = 256_000;
+    println!("Signed int32 {}", signed_int32);
+
+    let unsigned_int32: u32 = 256_000;
+    println!("Unsigned int32 {}", unsigned_int32);
+
+    let signed_int64: i64 = -256_000;
+    println!("Unsigned int64 {}", signed_int64);
+
+    let unsigned_int64: u64 = 256_000;
+    println!("Unsigned int64 {}", unsigned_int64);
+
+    let signed_int128: i128 = -256_000;
+    println!("Unsigned int128 {}", signed_int128);
+
+    let unsigned_int128: u64 = 256_000;
+    println!("Unsigned int128 {}", unsigned_int128);
 
     let binary_type: i8 = 0b0110;
     println!("binary type {}", binary_type);
